949361606c9de7c82bbe9f0b99fa687314e8855c
lrushikeswarreddy <lrushikeswarreddy@gmail.com>
2025-10-23 06:41:34 +0530

    Add Flyable and Swimmable interfaces with Duck class implementation

---
+++ Flyable.ts
@@ -0,0 +1,21 @@
+﻿interface Flyable {
+    fly(): void;
+}
+
+interface Swimmable {
+    swim(): void;
+}
+
+class Duck implements Flyable, Swimmable {
+    fly(): void {
+        console.log("Duck is flying through the air");
+    }
+
+    swim(): void {
+        console.log("Duck is swimming in the water");
+    }
+}
+
+const d = new Duck();
+d.fly();
+d.swim();
