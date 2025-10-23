e3798a5be49266ce0480de08d4a76e7764428cb8
lrushikeswarreddy <lrushikeswarreddy@gmail.com>
2025-10-23 06:30:37 +0530

    Implement abstract class for animal actions and concrete Dog class with sound method

---
+++ performAnimalActions.ts
@@ -0,0 +1,24 @@
+﻿// Abstract class with abstract and concrete methods
+abstract class performAnimalActions {
+    constructor(public name: string) {}
+
+    // Abstract method
+    abstract makeSound(): void;
+
+    // Concrete method
+    move(): void {
+        console.log(`${this.name} is moving...`);
+    }
+}
+
+// Concrete class that implements the abstract class
+class DogEx1 extends performAnimalActions {
+    makeSound(): void {
+        console.log("Woof ! Woof!");
+    }
+}
+
+// Usage
+const dog1 = new DogEx1('Tommy');
+dog1.makeSound();
+dog1.move();
